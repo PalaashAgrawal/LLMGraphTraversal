34ac30f1ebb525835c93e7390ce95188d819009f
palaashagrawal <palaashagrawal29@gmail.com>
2023-09-18 21:11:00 +0800

    level 3 complete

--- graph_gen_2.0/graph_gen.py
+++ graph_gen_2.0/graph_gen.py
@@ -29,6 +29,7 @@ class create_level():
 
         f = self.get_level(level)
         graph = f(self.n)
+        # self.n = len(graph.nodes) #sometimes, we add more nodes that n. Eg, level 3
         
 
 
@@ -36,13 +37,16 @@ class create_level():
         adj_matrix = nx.adjacency_matrix(graph, list(range(len(graph.nodes))))
         adj_array = np.array(adj_matrix.todense())
         
-        node_order = list(range(self.n))
+        
+
+        node_order = list(range(len(graph.nodes)))
         if self.is_jumbled: adj_array, node_order = self.jumble_adj_matrix(adj_array)
 
         mapping = {k: chr(65 + i) for k,i in enumerate(node_order)}  # 65 is ASCII for 'A'
         graph = nx.relabel_nodes(graph, mapping)
 
         shortest_path = self.get_shortest_path(graph, mapping[0], mapping[self.n-1])
+    
         
         out = self.convert_adjArray_to_str(adj_array, dict(sorted(mapping.items(), key=lambda x:x[1])))
 
@@ -73,32 +77,66 @@ class create_level():
                 return graph
 
             return _get_level2
-
         
+        if level ==3:
+            def _get_level3(n):
+                f'use a random tree, then add random paths between random nodes in the shortest path of the original graph. '
+                graph = nx.random_tree(n, seed = int(10000*random.random()))
+                graph = self._get_ordered_graph(graph)
+
+                shortest_path = nx.shortest_path(graph, source=0, target=n-1, weight=None, method='dijkstra')
+
+                num_extra_paths = random.randint(1,2) #we want at max 2 additional paths in the graph
+                for _ in range(num_extra_paths):
+                    #decide what nodes to connect with an extra path
+                    nodes_to_connect = random.sample(shortest_path, 2)
+                    start_node, end_node = nodes_to_connect
+                    #decide how many extra nodes should be there in the path that we are creating.
+                    num_extra_nodes = random.randint(1, len(shortest_path))
+                    #now create the new path
+                    new_node = len(graph.nodes)
+                    for i in range(num_extra_nodes):
+
+                        graph.add_edge(start_node, new_node)
+                        start_node = new_node
+                        new_node+=1
+                    graph.add_edge(start_node, end_node)
+
+                    
+                
+                #relabel the nodes in order through a DFS traversal
+                
+                return graph
+
+            return _get_level3
 
 
-    
+     
     
     def get_shortest_path(self, graph, source, target):
         shortest_path = nx.shortest_path(graph, source=source, target=target, weight=None, method='dijkstra')
         ret = f''
         for node in shortest_path[:-1]: ret+=f'{node} -> '
-        return ret+shortest_path[-1] 
+        return ret+str(shortest_path[-1])
         
 
     def jumble_adj_matrix(self, matrix, is_directed = False):
         n = len(matrix)
-        
         # Get a list of nodes and shuffle them to get a new sequence
         nodes = list(range(n))
         shuffled_nodes = self._shuffle_list(nodes)
-        
-        # Create a new adjacency matrix based on the shuffled sequence
-        new_matrix = np.zeros((n, n), dtype=int)
-        for i in range(n-1):
-            new_matrix[shuffled_nodes[i]][shuffled_nodes[i+1]] = 1
-            if not is_directed: new_matrix[shuffled_nodes[i+1]][shuffled_nodes[i]] = 1 #check logic later.
+        ordered_to_shuffled_mapping = {k:i for k,i in enumerate(shuffled_nodes)}
 
+
+        
+        new_matrix = np.zeros((n, n), dtype=int)
+        for i in range(n): #row
+            for j in range(n): #column
+                new_matrix[ordered_to_shuffled_mapping[i]][ordered_to_shuffled_mapping[j]] = matrix[i][j]
+                
+        
+
+            
         
         return new_matrix, shuffled_nodes
     
@@ -120,20 +158,7 @@ class create_level():
         return graph
 
 
-
-
-
-
-
-
-
-
-gen = create_level(n=10)
-out, sh = gen.create_graph(level = 2)
+gen = create_level(n=10, is_jumbled=True)
+out, sh = gen.create_graph(level = 3)
 print(out)
 print(sh)
-
-# gen = create_level(6, is_jumbled=True)
-# out, sh =gen.create_graph(1)
-# print(out)
-# print(sh)
